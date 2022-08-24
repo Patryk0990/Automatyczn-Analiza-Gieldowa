732fe5a65d53b4e1b5a606eea5c237aa1e3861b1
Patryk0990 <patrol0990@gmail.com>
2022-08-24 14:38:36 +0200

    Delete misleading variable values and unnecessary file

--- Stock/config.py
+++ Stock/config.py
@@ -1,4 +1,4 @@
 APCA_API_BASE_URL = "https://paper-api.alpaca.markets"
 APCA_API_DATA_URL = "https://data.alpaca.markets/v2"
-APCA_API_KEY_ID = "PKRRJKV1ZGTL605U4D5D"
-APCA_API_SECRET_KEY = "RvvCVuidlg36WL3nHMPe1EcHbPOMewgMa259jZ9p"
+APCA_API_KEY_ID = ""
+APCA_API_SECRET_KEY = ""
--- config.py
+++
@@ -1,4 +0,0 @@
-APCA_API_BASE_URL = "https://paper-api.alpaca.markets"
-APCA_API_DATA_URL = "https://data.alpaca.markets/v2"
-APCA_API_KEY_ID = ""
-APCA_API_SECRET_KEY = ""
